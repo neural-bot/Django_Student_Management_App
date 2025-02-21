9b0471ce64e82231a5d4571d23b3610216659523
Md Najib Ul Azam Mahi <najibul.me@gmail.com>
2025-02-21 10:00:45 +0600

    Update settings.py

--- stu_ms/settings.py
+++ stu_ms/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qiepyz)%kwhgp%&@2+7ut!-v%q_+f4jlqw68wtn6&oe=2obf8w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
